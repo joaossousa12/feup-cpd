04a3ca92d33e7e79ff4ba5f908a1b17188236baa
up202108738 <up202108738@up.pt>
2024-04-15 10:24:15 +0100

    Starting server

---
+++ assign2/src/Server.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.nio.channels.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class Server {
+    private static String db = "./doc/database.csv";
+
+    private ServerSocketChannel serverSocketChannel;
+
+    public Server(int port) throws IOException { 
+
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.socket().bind(new InetSocketAddress(port));
+
+        System.out.println("Server created, starting...");
+        run();
+
+    }
+
+    public void run() throws IOException {
+    }
+
+
+
+    
+}
